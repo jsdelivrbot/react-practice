fa89dab7fd31e36dd7504742129130232e5760f0
marcandy <marcandy.nj@gmail.com>
2017-01-17 14:53:50 -0700

    react

--- react-friends-CRA/friends/components/friendsList.js
+++ react-friends-CRA/friends/components/friendsList.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
-import './App.css';
 
-class App extends Component {
+
+class FriendsList extends Component {
+
+  handleChange(field, event) {
+    this.setState( { [field]: event.target.value })
+  }
+
   constructor(props) {
-    
+    super(props);
+
+    this.state = {
+      searcText: ""
+      , orderBy: "name"
+      , order: "ascending"
+    };
   }
   render() {
     return (
@@ -11,14 +22,23 @@ class App extends Component {
     <form className="form-inline searchForm" role="form">
         <div className="form-group">
 
-            <input className="form-control" placeholder="Search For Friends" />
+            <input className="form-control"
+            onChange={ this.handleChange.bind( this, "searchText" )}
+            placeholder="Search For Friends"
+            value={this.state.searchText}
+            />
 
-            <select className="input-medium">
+            <select className="input-medium"
+              onChange={this.}
+              value={ this.state.orderBy}
+            >
                 <option>Name</option>
                 <option>#Friends</option>
             </select>
 
-            <select className="input-medium">
+            <select className="input-medium"
+              value={ this.state.order}
+            >
                 <option>Descending</option>
                 <option>Ascending</option>
             </select>
@@ -33,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default FriendsList;
