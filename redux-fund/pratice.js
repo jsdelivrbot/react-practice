0bda3ad9fdd35df624d6386e7c9e7d680cb4e84e
marcandy <marcandy.nj@gmail.com>
2017-01-19 21:42:32 -0700

    redux

--- redux-fund/pratice.js
+++ redux-fund/pratice.js
@@ -1,25 +1,75 @@
-function counter(state, action) {
-  if(action.type === 'INCREMENT'){
-    return state + 1;
-  } else if (action.type === 'DECREMENT') {
-    return state - 1;
+// function counter(state, action) {
+//   if(action.type === 'INCREMENT'){
+//     return state + 1;
+//   } else if (action.type === 'DECREMENT') {
+//     return state - 1;
+//   }
+// }
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'INCREMENT':
+      return state - 1;
+    default:
+      return state;
   }
 }
 
-expect(
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1);
+const { createStore } = Redux;
+const store = createStore(counter);
 
-expect(
-  counter(1, { type: 'INCREMENT' })
-).toEqual(2);
+const render = () => {
+  document.body.innerText = store.getState();
+};
 
-expect(
-  counter(2, { type: 'DECREMENT' })
-).toEqual(1);
+store.subscribe(render);
+render();
 
-expect(
-  counter(1, { type: 'DECREMENT' })
-).toEqual(0);
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
 
-console.log('Tests passed!')
+
+// createStore
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+  state = reducer(state, action);
+  listeners.forEach(listener => listener());
+};
+
+  const subscribe = (listener) => {
+  listeners.push(listener);
+  return () => {
+    listeners = listeners.filter(l => l !== listener);
+  };
+};
+  dispatch({});
+  
+  return { getState, dispatch, subscribe };
+};
+
+
+
+// expect(
+//   counter(0, { type: 'INCREMENT' })
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, { type: 'INCREMENT' })
+// ).toEqual(2);
+//
+// expect(
+//   counter(2, { type: 'DECREMENT' })
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, { type: 'DECREMENT' })
+// ).toEqual(0);
+//
+// console.log('Tests passed!')
